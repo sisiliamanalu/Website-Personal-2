7bc11340f12f39afac201c4b1b7bb57426cbf372
sisiliamanalu <153351609+sisiliamanalu@users.noreply.github.com>
2025-07-30 18:53:07 +0700

    Update app.py

--- app.py
+++ app.py
@@ -81,5 +81,5 @@ def extract():
     except Exception as e:
         return jsonify({"error": str(e)})
 
-if __name__ == '__main__':
-    app.run(debug=True)
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", debug=True)
